cf4c52feaf58f60d21b064bd603c0017feeb3844
etienne <etienne.brocherieux@viacesi.fr>
2025-11-20 16:34:04 +0100

    infrastructure

---
+++ infrastructure/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.75"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "random_id" "suffix" {
+  byte_length = 6
+}
+
+
+#Resource Group
+module "resource_group" {
+  source = "./modules/ressource_group"
+  resource_group_name = "Cloud-computing-project"
+  location            = var.location
+  suffix             = random_id.suffix.hex
+}
+
+#Virtual Network
+module "virtual_network" {
+  source              = "./modules/virtual_network"
+  resource_group_name = module.resource_group.name
+  location            = var.location
+  vnet_name           = "vnet-10-0-0-0-16"
+  address_space       = "10.0.0.0/16"
+}
+
+#Database
+module "database" {
+  source              = "./modules/database"
+  database_name       = "counter"
+  resource_group_name = module.resource_group.name
+  location            = var.location
+  admin_user          = "adminuser"
+  admin_password      = "P@ssword123"
+  subnet_id           = module.virtual_network.subnets[1]  # ID du subnet 'database-subnet'
+  suffix             = random_id.suffix.hex
+}
+
